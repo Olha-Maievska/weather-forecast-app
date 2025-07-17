a199f7185d090289e4a6f4fbd76b6274d54051db
Olha Maievska <olhamaievska91@gmail.com>
2025-07-17 18:14:42 +0200

    feat: edit app component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Forecast from "./components/Forecast";
 import ForecastChart from './components/ForecastChart';
 import Header from "./components/Header";
 
-
 function App() {
   const night = new Date().getHours() >= 18 || new Date().getHours() < 6;
 
